b1cf14282460044817d75888379feac6c49862c8
Lukin <luxiansheng@mogoroom.com>
2018-02-05 20:07:25 +0800

    添加爬取我爱我家

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ install:
 	python3 -m venv .virtualenv
 	./ve pip install --upgrade pip
 	./ve pip install -r requirements.txt
+
+price:
+	./ve python price.py
--- price.py
+++ price.py
@@ -27,7 +27,7 @@ DB_PORT = int(LazyFW.config('DB', 'PORT'))
 FTQUEUE = Queue()
 
 # 浏览器里保存的cookies
-COOKIES = LazyFW.read_file(LazyFW.ABS_PATH + '/cookies.txt')
+COOKIES = LazyFW.read_file(LazyFW.ABS_PATH + '/cookies-5i5j.txt')
 
 
 def get_conn():
@@ -45,7 +45,7 @@ def create_db():
     # 创建表
     c.execute(
         r'''
-CREATE TABLE IF NOT EXISTS `urls` (
+CREATE TABLE IF NOT EXISTS `price_urls` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `url` varchar(255) NOT NULL DEFAULT '',
   `status` tinyint(1) NOT NULL DEFAULT '0',
@@ -68,6 +68,7 @@ CREATE TABLE IF NOT EXISTS `prices` (
   `price` decimal(10,2) NOT NULL DEFAULT 0 COMMENT '价格',
   `huxing` varchar(50) NOT NULL DEFAULT '' COMMENT '户型',
   `qianyue` varchar(50) NOT NULL DEFAULT '' COMMENT '签约时间',
+  `zhuangxiu` varchar(50) NOT NULL DEFAULT '' COMMENT '装修类型',
   PRIMARY KEY (`id`),
   UNIQUE KEY `fang_id` (`fang_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -82,7 +83,7 @@ def url_insert(url):
     DB_CONN = get_conn()
     c = DB_CONN.cursor()
     try:
-        c.execute(r'''INSERT INTO `urls` (`url`) VALUES(%s);''', (url,))
+        c.execute(r'''INSERT INTO `price_urls` (`url`) VALUES(%s);''', (url,))
         result = True
     except mysql.IntegrityError:
         result = None
@@ -99,7 +100,7 @@ def url_update(url, status):
     DB_CONN = get_conn()
     c = DB_CONN.cursor()
     try:
-        c.execute(r'''UPDATE `urls` SET `status`=%s WHERE `url`=%s;''',
+        c.execute(r'''UPDATE `price_urls` SET `status`=%s WHERE `url`=%s;''',
                   (status, url,))
         result = True
     except mysql.IntegrityError:
@@ -118,16 +119,38 @@ def addInfo(info):
     c = DB_INSERT.cursor()
     try:
         c.execute(
-            r'''INSERT INTO `prices` (`fang_id`,`xiaoqu_url`,`xiaoqu_name`,`city`,`area`,`price`,`huxing`,`qianyue`) VALUES(%s,%s,%s,%s,%s,%s,%s,%s);''',
+            r'''INSERT INTO `prices` (`fang_id`,`xiaoqu_url`,`xiaoqu_name`,`city`,`area`,`price`,`huxing`,`qianyue`,`zhuangxiu`) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s);''',
             (info['fang_id'], info['xiaoqu_url'], info['xiaoqu_name'], info['city'], info['area'], info['price'],
-             info['huxing'], info['qianyue']))
+             info['huxing'], info['qianyue'], info['zhuangxiu']))
         result = True
     except mysql.IntegrityError:
         c.execute(
-            r'''UPDATE `prices` SET `xiaoqu_url`=%s,`xiaoqu_name`=%s,`city`=%s,`area`=%s,`price`=%s, `huxing`=%s, `qianyue`=%s WHERE `fang_id`=%s;''',
+            r'''UPDATE `prices` SET `xiaoqu_url`=%s,`xiaoqu_name`=%s,`city`=%s,`area`=%s,`price`=%s, `huxing`=%s, `qianyue`=%s, `zhuangxiu`=%s WHERE `fang_id`=%s;''',
             (info['xiaoqu_url'], info['xiaoqu_name'], info['city'], info['area'], info['price'], info['huxing'],
-             info['qianyue'],
+             info['qianyue'], info['zhuangxiu'],
              info['fang_id'],))
+        result = True
+    except mysql.OperationalError:
+        result = None
+
+    c.close()
+    DB_INSERT.commit()
+    DB_INSERT.close()
+    return result
+
+
+def addInfoExt(info):
+    DB_INSERT = get_conn()
+    c = DB_INSERT.cursor()
+    try:
+        c.execute(
+            r'''INSERT INTO `prices` (`fang_id`,`city`,`area`,`zhuangxiu`,`qianyue`,`price`) VALUES(%s,%s,%s,%s,%s,%s);''',
+            (info['fang_id'], info['city'], info['area'], info['zhuangxiu'], info['qianyue'], info['price']))
+        result = True
+    except mysql.IntegrityError:
+        c.execute(
+            r'''UPDATE `prices` SET `area`=%s,`city`=%s,`zhuangxiu`=%s,`qianyue`=%s,`price`=%s WHERE `fang_id`=%s;''',
+            (info['area'], info['city'], info['zhuangxiu'], info['qianyue'], info['price'], info['fang_id'],))
         result = True
     except mysql.OperationalError:
         result = None
@@ -174,7 +197,7 @@ def fetch(url):
                 xiaoqu_url = d('div.detail-main div.infomain a.infotit').attr('href')
                 xiaoqu_name = d('div.detail-main div.infomain a.infotit').text()
                 city = re.sub(r'^.+://([^\.]+)(.+)?$', r'\1', url)
-                area = 'NA'
+                area = 0
                 price = re.sub(r'[^\d]', r'', d('div.house-info p.cjinfo').eq(0).text())
                 huxing = d('h1.house-tit').text().split(' ')[1]
                 qianyue = d('div.house-info p.cjinfo').eq(1).text()
@@ -189,8 +212,8 @@ def fetch(url):
                     "price": price,
                     "huxing": huxing,
                     "qianyue": qianyue,
+                    "zhuangxiu": '--',
                 }
-
                 insert_s = addInfo(data)
                 if insert_s:
                     LazyFW.log(
@@ -200,15 +223,29 @@ def fetch(url):
 
                 # 收集当前小区的其他房源和面积
                 for ele in d('ul.yizucontent').items():
-                    fang_id = ele.find('li').eq(0).find('a').attr('href')
+                    fang_url = ele.find('li').eq(0).find('a').attr('href')
+                    fang_id = re.sub(r'^.+/(\d+)\.html(?:.+)?$', r'\1', fang_url)
                     area = re.sub(r'[^\d]', r'', ele.find('li').eq(1).text())
-                    insert_s = addInfo({
+                    city = re.sub(r'^.+://([^\.]+)(.+)?$', r'\1', fang_url)
+                    zhuangxiu = ele.find('li').eq(2).text()
+                    qianyue = ele.find('li').eq(3).text()
+                    price = re.sub(r'[^\d]', r'', ele.find('li').eq(4).text())
+
+                    ele_data = {
                         "fang_id": fang_id,
                         "area": area,
-                    })
+                        "city": city,
+                        "zhuangxiu": zhuangxiu,
+                        "qianyue": qianyue,
+                        "price": price,
+                    }
+                    insert_s = addInfoExt(ele_data)
                     if insert_s:
                         LazyFW.log(
-                            "Update Data: fang_id: %s, area: %s" % (data['fang_id'], data['area'],))
+                            "Update Data: fang_id: %s, area: %s, city: %s, zhuangxiu: %s, qianyue: %s, price: %s" % (
+                                ele_data['fang_id'], ele_data['area'], ele_data['city'], ele_data['zhuangxiu'],
+                                ele_data['qianyue'],
+                                ele_data['price'],))
 
             # 收集网址
             urls = LazyFW.get_urls(scan_html)
@@ -237,6 +274,7 @@ def fetch(url):
     except Exception as e:
         LazyFW.log("Error: %s" % (e,))
         url_update(url, 2)
+        raise
         return False
 
     return True
@@ -272,9 +310,9 @@ def main():
 
     # 再次查询出数据
     DB_CONN = get_conn()
-    LazyFW.log(r'''SELECT `url` FROM `urls` where `status`=0;''')
+    LazyFW.log(r'''SELECT `url` FROM `price_urls` where `status`=0;''')
     c = DB_CONN.cursor()
-    c.execute(r'''SELECT `url` FROM `urls` where `status`=0;''')
+    c.execute(r'''SELECT `url` FROM `price_urls` where `status`=0;''')
     for row in c.fetchall():
         FTQUEUE.put_nowait(row[0])
 
